65c1aa4871f0b5de887c3bb689bff3eaad031629
{harsha} <{harsha2004@gmail.com}>
2026-02-27 00:13:31 +0530

    Initial commit

--- src/middleware/error.middleware.js
+++ src/middleware/error.middleware.js
@@ -2,5 +2,5 @@ module.exports = (err, req, res, next) => {
     console.error(err);
     res
         .status(err.status || 500)
-        . json({ message: err.message || "Server Error" });
+        .json({ message: err.message || "Server Error" });
 }
